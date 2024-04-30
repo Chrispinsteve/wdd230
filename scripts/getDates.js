d906d91096d67561e33567f851b0b763ccc746b7
Steve <129707522+Chrispinsteve@users.noreply.github.com>
2024-04-30 17:13:53 -0400

    Update getDates.js

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,3 +2,5 @@ const Options = {
     month: 'long', day: 'numeric', year: 'numeric'};
 
 document.querySelector('#date').textContent = new Date().toLocaleDateString('en-Us', Options);
+
+document.querySelector('#lastModified').textContent = new Date().toDateString('en-Us', Options)
