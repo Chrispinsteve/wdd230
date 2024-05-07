32ff05755b06c766b7e0c1b7e6d3dca54c4b0b64
Steve <129707522+Chrispinsteve@users.noreply.github.com>
2024-05-07 17:51:03 -0400

    Update chamber.js

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -5,3 +5,34 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 })
+
+
+/*-------------- "DARK" and "LIGHT" --------------*/
+const modeButton = document.querySelector('#mode');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const divGeneral = document.querySelector('.general');
+const leftSide = document.querySelector('.left-side');
+const section = document.querySelector('section');
+const article = document.querySelector('article');
+const h1 = document.querySelector('h1');
+const paragraph = document.querySelector('p');
+
+
+modeButton.addEventListener('click', () => {
+    if(modeButton.textContent.includes('🌙')) {
+
+        divGeneral.style.background = '#363638';
+        leftSide.style.background = 'rgb(190, 190, 190)'
+        main.style.background = '#797979'
+        h1.style.color = 'white'
+
+        modeButton.textContent = '🔆'
+    }
+    else {
+        divGeneral.style.background = 'whitesmoke'
+        main.style.background = 'rgb(226, 226, 226)';
+        h1.style.color = 'rgb(68, 68, 68)'
+        modeButton.textContent = '🌙';
+    }
+})
