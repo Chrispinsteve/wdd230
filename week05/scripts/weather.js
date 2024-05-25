75b8d8e891f58a30af281a28befddacd60168ec5
Steve <129707522+Chrispinsteve@users.noreply.github.com>
2024-05-25 18:46:44 -0400

    weather update

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,1 +1,6 @@
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
+
+//Select HTML elements in the the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
