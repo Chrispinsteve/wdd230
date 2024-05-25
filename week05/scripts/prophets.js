fefe5c0fa562fa81b04f3485499955edbdbfb469
Steve <129707522+Chrispinsteve@users.noreply.github.com>
2024-05-25 14:06:12 -0400

    update

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -4,34 +4,31 @@ const cards = document.querySelector('#cards');
 async function getProphetData(url) {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets);
     displayProphets(data.prophets);
 }
-getProphetData();
+
+getProphetData(url);
 
 const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
-
         //Create element to add to div.cards element
         let card = document.createElement('section');
-        let fullName = document.createElement('__');
+        let fullName = document.createElement('h2');
         let portrait = document.createElement('img');
-
-
+        
         //Build h2 content out to show prophet's full name
-        fullName.textContent = `${prophets._____} ________`;
-
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        
         //Build the image portrait by setting all the relevant attributes
-        portrait.setAttribute('src', prophets.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${prophets.____} ____________`);
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
-        portrait.setAttribute('heith', '440');
-
+        portrait.setAttribute('height', '440');
+        
         // Append the section(card) with the created elements
-        card.appendChild(_________) // fill in the blank
+        card.appendChild(fullName);
         card.appendChild(portrait);
-
-        card.appendChild(card);
+        cards.appendChild(card);
     });
 }
