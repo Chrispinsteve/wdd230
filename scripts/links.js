166ba8d9bd0bdc17e7c7c43cc153b0a72f1a6af3
Steve <129707522+Chrispinsteve@users.noreply.github.com>
2024-05-26 14:57:06 -0400

    update

--- scripts/links.js
+++ scripts/links.js
@@ -1,3 +1,6 @@
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=18.53921&lon=-72.335&appid=9a91868ee8db995a0699a868f975dfe6&units=imperial';
+
 const baseURL = "https://chrispinsteve.github.io/wdd230/";
 const linksURL = "https://chrispinsteve.github.io/wdd230/data/links.json";
 
