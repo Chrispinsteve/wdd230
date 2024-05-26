14d08af785dd2bd138b445c03042b1ac10f15ed7
Steve <129707522+Chrispinsteve@users.noreply.github.com>
2024-05-26 15:26:30 -0400

    update

--- scripts/links.js
+++ scripts/links.js
@@ -1,13 +1,19 @@
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=18.53921&lon=-72.335&appid=9a91868ee8db995a0699a868f975dfe6&units=imperial';
-
 const baseURL = "https://chrispinsteve.github.io/wdd230/";
 const linksURL = "https://chrispinsteve.github.io/wdd230/data/links.json";
 
 
+// Select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const linkList = document.querySelector('#Activities')
 
-const linkList = document.querySelector('')
 
+
+
+
+
+//Data WEEKS INFORMATION
 async function getLinks() {
     const response = await fetch(linksURL);
     let data = await response.json();
