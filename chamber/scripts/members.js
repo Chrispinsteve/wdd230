10fbf01d5af133c322f1a48b24bb2494de3c747a
Steve <129707522+Chrispinsteve@users.noreply.github.com>
2024-05-29 13:50:10 -0400

    update

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -2,10 +2,10 @@
 const linkMembers = "https://github.com/Chrispinsteve/wdd230/blob/main/chamber/data/members.json";
 
 //Get into the html document
-const  article = document.querySelector('.article');
+const article = document.querySelector('.article');
 const image = document.querySelector('#image-main');
 
-async function displayLinks(){
+async function displayLinks() {
     const response = await fetch(linkMembers);
     const data = await response.json();
     console.table(data)//display data 
@@ -13,11 +13,13 @@ async function displayLinks(){
 }
 
 
-function discoverMember(){
+function discoverMember() {
     Members.forEach(member => {
         let memberItem = document.createElement('li');
         let memberName = document.createElement('h3');
+        let imageSrc = member.image;
 
+        image.setAttribute('src', imageSrc)
         memberName.textContent = member.name;
         memberItem.appendChild(memberName);
     })
